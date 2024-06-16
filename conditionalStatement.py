38388476f4796f48e1c163ae03957fd895ea8011
kdh95 <ehgks3323@gmail.com>
2024-06-16 21:59:56 +0900

    conditionalStatement update

--- conditionalStatement.py
+++ conditionalStatement.py
@@ -14,3 +14,15 @@ else:
 # <=
 # ==
 # !=
+
+if height > 120:
+    print("You can ride the rollercoaster!")
+    age = int(input("What is your age?"))
+    if age <= 18:
+        print("Please pay $7")
+    elif age > 12:
+        print("Please pay $14")
+    else:
+        print("Please pay $18")
+else:
+    print("Sorry, you have to grow taller before you can ride.")
