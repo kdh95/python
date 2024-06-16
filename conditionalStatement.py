621a557b38475f6a7c405023a6349a0a508f083b
kdh95 <ehgks3323@gmail.com>
2024-06-16 22:17:31 +0900

    conditionalStatement update

--- conditionalStatement.py
+++ conditionalStatement.py
@@ -26,3 +26,21 @@ if height > 120:
         print("Please pay $18")
 else:
     print("Sorry, you have to grow taller before you can ride.")
+
+
+
+# 연습문제 윤년 계산하기
+# 4로 나누어 떨어지는 해는 윤년으로 한다.
+# 연수가 4,100 으로 나누어 떨어지는 해는 평년으로 한다.
+# 4, 100, 300 으로 나누어 떨어지는 해는 윤년으로 둔다.
+# Which year do you want to check?
+year = int(input())
+
+if year % 4 == 0 and year % 100 == 0 and year % 300 == 0:
+  print("Leap year")
+elif year % 4 == 0 and year % 100 == 0:
+  print("Not leap year")
+elif year % 4 == 0:
+  print("Leap year")
+else:
+  print("Not leap year")
