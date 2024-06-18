d442046eaf5486e54004ff911b96177d27f05e9e
kdh95 <ehgks3323@gmail.com>
2024-06-18 23:44:55 +0900

    loops update

---
+++ loops.py
@@ -0,0 +1,15 @@
+# For Loop with Lists
+fruits = ["Apple", "Peach", "Pear"]
+
+for fruit in fruits: # 명령어를 원하는 만큼 반복해준다.
+    print(fruit)
+
+
+
+
+# For Loop with Range
+for number in range(1, 10): # 10은 포함하지 않는다.
+    print(number)
+
+for number in range(1, 11, 3): # 3씩 증가한다.
+    print(number)
