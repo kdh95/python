58180a0488a67bbba699ccb1f6323d67d1c8665f
kdh95 <ehgks3323@gmail.com>
2024-06-25 14:55:05 +0900

    loops & functions update

---
+++ function.py
@@ -0,0 +1,24 @@
+# 파이썬 함수
+# 함수뒤에는 괄호 ()가 붙는다.
+print("Hello") # print 함수
+num_char = len("Hello")
+print(num_char)
+
+# 나만의 함수 만들기
+def my_function():
+    print("Hello")
+    print("Bye")
+
+
+my_function() # 함수 실행
+
+# Defining Functions
+# def my_function():
+#     Do this
+#     Then do this
+#     Finally do this
+
+# Calling Functions
+# my_functions()
+
+
--- loops.py
+++ loops.py
@@ -30,3 +30,8 @@ for num in range(1, 101):
     print("Buzz")
   else:
     print(num)
+
+
+
+
+
