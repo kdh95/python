c94896f5572f6ce56aeb7bdb0268af5861777807
kdh95 <ehgks3323@gmail.com>
2024-06-27 13:43:22 +0900

    hangMan update

--- hangMan.py
+++ hangMan.py
@@ -61,3 +61,28 @@ print(display)
 
 
 
+# 전체 코드 짜보기
+
+end_of_game = False
+
+word_list = ["aardvark", "baboon", "camel"]
+
+chosen_word = random.choice(word_list)
+
+display = []
+for _ in range(len(chosen_word)):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    for position in range(len(chosen_word)):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+
+    print(display)
+
+    if "_" not in display:
+        print("You Win")
+        end_of_game = True
