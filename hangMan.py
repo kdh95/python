df153ff6068387a849c5dde4539316c018c07bad
kdh95 <ehgks3323@gmail.com>
2024-06-28 09:16:24 +0900

    hangMan update

--- hangMan.py
+++ hangMan.py
@@ -1,77 +1,67 @@
-# 행맨 게임을 만들어 보자
-# Step 1
+# # 행맨 게임을 만들어 보자
+# # Step 1
+# import random
+# word_list = ["aardvark", "baboon", "camel"]
+#
+# # TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+# chosen_word = random.choice(word_list)
+#
+# # TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+# user_input = input("Guess a letter: ").lower()
+#
+# # TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+# for letter in chosen_word:
+#     if letter == user_input:
+#         print("Right")
+#
+#     else:
+#         print("Wrong")
+#
+#
+#
+#
+# # Step 2
+# word_list = ["aardvark", "baboon", "camel"]
+# chosen_word = random.choice(word_list)
+#
+# # Testing code
+# print(f'Pssst, the solution is {chosen_word}.')
+#
+# # TODO-1: - Create an empty List called display.
+# # For each letter in the chosen_word, add a "_" to 'display'.
+# # So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"]
+# # with 5 "_" representing each letter to guess.
+# display = []
+# for _ in range(len(chosen_word)):
+#     display += "_"
+#
+# print(display)
+#
+# guess = input("Guess a letter: ").lower()
+#
+# # TODO-2: - Loop through each position in the chosen_word;
+# # If the letter at that position matches 'guess' then reveal that letter in the display at that position.
+# # e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
+# for position in range(len(chosen_word)):
+#     letter == chosen_word[position]
+#     if letter == guess:
+#         print("Right")
+#         display[position] = letter
+#     else:
+#         print("Wrong")
+# print(display)
+# # TODO-3: - Print 'display' and you should see the guessed letter in the correct position
+# # and every other letter replace with "_".
+# # Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
+
+
+
+
+
+
 import random
-word_list = ["aardvark", "baboon", "camel"]
-
-# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
-chosen_word = random.choice(word_list)
-
-# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-user_input = input("Guess a letter: ").lower()
-
-# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-for letter in chosen_word:
-    if letter == user_input:
-        print("Right")
-
-    else:
-        print("Wrong")
-
-
-
-
-# Step 2
-word_list = ["aardvark", "baboon", "camel"]
-chosen_word = random.choice(word_list)
-
-# Testing code
-print(f'Pssst, the solution is {chosen_word}.')
-
-# TODO-1: - Create an empty List called display.
-# For each letter in the chosen_word, add a "_" to 'display'.
-# So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"]
-# with 5 "_" representing each letter to guess.
-display = []
-for _ in range(len(chosen_word)):
-    display += "_"
-
-print(display)
-
-guess = input("Guess a letter: ").lower()
-
-# TODO-2: - Loop through each position in the chosen_word;
-# If the letter at that position matches 'guess' then reveal that letter in the display at that position.
-# e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
-for position in range(len(chosen_word)):
-    letter == chosen_word[position]
-    if letter == guess:
-        print("Right")
-        display[position] = letter
-    else:
-        print("Wrong")
-print(display)
-# TODO-3: - Print 'display' and you should see the guessed letter in the correct position
-#  and every other letter replace with "_".
-# Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
-
-
-
-
-
-
-
 
 # 전체 코드 짜보기
-
-end_of_game = False
-word_list = ["aardvark", "baboon", "camel"]
-chosen_word = random.choice(word_list)
-
-lives = 6
-
-display = []
-for _ in range(len(chosen_word)):
-    display += "_"
 
 stages = ['''
   +---+
@@ -130,12 +120,249 @@ stages = ['''
 =========
 ''']
 
+logo = ''' 
+ _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/    '''
+
+word_list = [
+'abruptly',
+'absurd',
+'abyss',
+'affix',
+'askew',
+'avenue',
+'awkward',
+'axiom',
+'azure',
+'bagpipes',
+'bandwagon',
+'banjo',
+'bayou',
+'beekeeper',
+'bikini',
+'blitz',
+'blizzard',
+'boggle',
+'bookworm',
+'boxcar',
+'boxful',
+'buckaroo',
+'buffalo',
+'buffoon',
+'buxom',
+'buzzard',
+'buzzing',
+'buzzwords',
+'caliph',
+'cobweb',
+'cockiness',
+'croquet',
+'crypt',
+'curacao',
+'cycle',
+'daiquiri',
+'dirndl',
+'disavow',
+'dizzying',
+'duplex',
+'dwarves',
+'embezzle',
+'equip',
+'espionage',
+'euouae',
+'exodus',
+'faking',
+'fishhook',
+'fixable',
+'fjord',
+'flapjack',
+'flopping',
+'fluffiness',
+'flyby',
+'foxglove',
+'frazzled',
+'frizzled',
+'fuchsia',
+'funny',
+'gabby',
+'galaxy',
+'galvanize',
+'gazebo',
+'giaour',
+'gizmo',
+'glowworm',
+'glyph',
+'gnarly',
+'gnostic',
+'gossip',
+'grogginess',
+'haiku',
+'haphazard',
+'hyphen',
+'iatrogenic',
+'icebox',
+'injury',
+'ivory',
+'ivy',
+'jackpot',
+'jaundice',
+'jawbreaker',
+'jaywalk',
+'jazziest',
+'jazzy',
+'jelly',
+'jigsaw',
+'jinx',
+'jiujitsu',
+'jockey',
+'jogging',
+'joking',
+'jovial',
+'joyful',
+'juicy',
+'jukebox',
+'jumbo',
+'kayak',
+'kazoo',
+'keyhole',
+'khaki',
+'kilobyte',
+'kiosk',
+'kitsch',
+'kiwifruit',
+'klutz',
+'knapsack',
+'larynx',
+'lengths',
+'lucky',
+'luxury',
+'lymph',
+'marquis',
+'matrix',
+'megahertz',
+'microwave',
+'mnemonic',
+'mystify',
+'naphtha',
+'nightclub',
+'nowadays',
+'numbskull',
+'nymph',
+'onyx',
+'ovary',
+'oxidize',
+'oxygen',
+'pajama',
+'peekaboo',
+'phlegm',
+'pixel',
+'pizazz',
+'pneumonia',
+'polka',
+'pshaw',
+'psyche',
+'puppy',
+'puzzling',
+'quartz',
+'queue',
+'quips',
+'quixotic',
+'quiz',
+'quizzes',
+'quorum',
+'razzmatazz',
+'rhubarb',
+'rhythm',
+'rickshaw',
+'schnapps',
+'scratch',
+'shiv',
+'snazzy',
+'sphinx',
+'spritz',
+'squawk',
+'staff',
+'strength',
+'strengths',
+'stretch',
+'stronghold',
+'stymied',
+'subway',
+'swivel',
+'syndrome',
+'thriftless',
+'thumbscrew',
+'topaz',
+'transcript',
+'transgress',
+'transplant',
+'triphthong',
+'twelfth',
+'twelfths',
+'unknown',
+'unworthy',
+'unzip',
+'uptown',
+'vaporize',
+'vixen',
+'vodka',
+'voodoo',
+'vortex',
+'voyeurism',
+'walkway',
+'waltz',
+'wave',
+'wavy',
+'waxy',
+'wellspring',
+'wheezy',
+'whiskey',
+'whizzing',
+'whomever',
+'wimpy',
+'witchcraft',
+'wizard',
+'woozy',
+'wristwatch',
+'wyvern',
+'xylophone',
+'yachtsman',
+'yippee',
+'yoked',
+'youthful',
+'yummy',
+'zephyr',
+'zigzag',
+'zigzagging',
+'zilch',
+'zipper',
+'zodiac',
+'zombie',
+]
 
 
+end_of_game = False
+chosen_word = random.choice(word_list)
 
+lives = 6
+
+display = []
+for _ in range(len(chosen_word)):
+    display += "_"
+
+print(logo)
 
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
+
+    if guess in display:
+        print(f"You've already guessed {guess}")
 
     for position in range(len(chosen_word)):
         letter = chosen_word[position]
@@ -143,6 +370,7 @@ while not end_of_game:
             display[position] = letter
 
     if guess not in chosen_word:
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
         lives -= 1
         if lives == 0:
             end_of_game = True
