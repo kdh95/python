db519162aeec75f92ae040b4ecd921ea844bd222
kdh95 <ehgks3323@gmail.com>
2024-07-01 20:27:19 +0900

    dictionaries update

---
+++ dictionaries.py
@@ -0,0 +1,10 @@
+# 파이썬 딕셔너리는 실제 사전(딕셔너리)와 비슷하게 동작한다.
+# 모든 딕셔너리는 다음과 같이 정의할 수 있다.
+# {key : value}
+
+programming_dictionaries = {
+    "Bug" : "An error  in program that prevents the program from running as expected",
+    "Function" : "A piece of code that you can easily call over and over again",
+}
+
+print(programming_dictionaries["Bug"])
