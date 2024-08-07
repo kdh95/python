8ffe41a6e5adb8936ae5010880ed26d16ac6d026
kdh95 <ehgks3323@gmail.com>
2024-08-07 11:42:11 +0900

    GUI update

---
+++ Turtle/main.py
@@ -0,0 +1,20 @@
+from turtle import Turtle, Screen
+
+timmy_turtle = Turtle()
+
+# 거북이 모양 노출 가능
+timmy_turtle.shape("turtle")
+timmy_turtle.color("red") # 색상 변경
+
+
+# 거북이가 특정 동작을 하게 하기
+timmy_turtle.forward(100)
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
